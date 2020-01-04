71c6ebe68d019df8d3f4e55ed7cc40e908d61af3
kirill <thegaldren@gmail.com>
2020-01-04 09:54:05 +0300

    fix: can't read and write file at the same time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -46,10 +46,12 @@ public class Main {
     // based on https://github.com/apache/commons-imaging/blob/master/src/test/java/org/apache/commons/imaging/examples/WriteExifMetadataExample.java
     private static void setPhotoMetadata(String photo, Exposure exposureData) {
 
+        File dest = new File(photo.substring(0, photo.lastIndexOf("/") + 1) + exposureData.number + ".jpg");
+
         File file = new File(photo);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy:MM:dd hh:mm:ss");
 
-        try (FileOutputStream fos = new FileOutputStream(file);
+        try (FileOutputStream fos = new FileOutputStream(dest);
              OutputStream os = new BufferedOutputStream(fos)){
 
             TiffOutputSet outputSet = null;
